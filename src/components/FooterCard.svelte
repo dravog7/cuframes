d49c4c9c7cd6d2c0c0d806da18651cd0f80badda
vishnu.k <vishnu.k@BBMCDVISHNUK.local>
2021-07-31 16:01:23 +0530

    some styles added

--- src/components/FooterCard.svelte
+++ src/components/FooterCard.svelte
@@ -1,6 +1,6 @@
 <script>
 </script>
-<div class="order-4 md:order-3 flex justify-center content-center text-center font-bold text-gray-500 py-5 md:w-1/2">
+<div class="order-4 md:order-3 flex md:ml-28 text-xl  font-normal text-gray-500 py-8 md:w-1/3">
 <span>Crafted with love <span class="text-red-600">&#10084;</span> and sulaimani</span><img class="h-4 pl-2" src="./sulaimani.png" alt="tea"/>
 </div>
 <style lang="postcss">
--- src/components/TitleCard.svelte
+++ src/components/TitleCard.svelte
@@ -14,34 +14,34 @@
   }
 </style>
 
-<div class="order-1 flex flex-col justify-around py-8 px-2 h-v80
-            md:w-1/2 md:h-auto">
+<div class="order-1 md:ml-24 flex flex-col justify-around py-2 px-1 md:h-5/6
+            md:w-1/3 md:h-auto">
   <div class="flex flex-col justify-around px-8" style="flex:2;">
     <div>
       <img class="h-14" src="Logo.png" alt="logo" />
     </div>
     <div>
-      <h1 class="font-black text-5xl py-1">cutframe</h1>
-      <h2 class="font-black text-gray-400 text-3xl">wireframe kit</h2>
+      <h1 class="font-bold text-5xl md:text-7xl py-1">cutframe</h1>
+      <h2 class="font-bold text-gray-400 text-6xl">wireframe kit</h2>
     </div>
-    <p>
+    <p class="md:text-2xl font-light text-gray-400 ">
       An open source wireframe kit for designers which include 350+ components and
       200+ available in both light and dark mode.
     </p>
   </div>
-  <div class="flex flex-col justify-around flex-1 
+  <div class="flex flex-col  flex-1 
               md:flex-row md:content-center md:flex-initial">
     <a
       href="https://ko200.github.io/cuframes/"
       class="button bg-black text-white visited:text-white">
       <img src="./figma.png" alt="figma" />
-      <span>Get it now</span>
+      <span class="text-xl">Get it now</span>
     </a>
     <a
       href="https://www.buymeacoffee.com/vishnukoux"
       class="button bg-gray-200 text-black visited:text-black">
       <img src="./Coffee.png" alt="coffee" />
-      <span>Buy me a coffee</span>
+      <span class="text-xl">Buy me a coffee</span>
     </a>
   </div>
 </div>
