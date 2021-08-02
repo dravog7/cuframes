b57a87d9ce12b1576552f391674014588fe8f07f
Ko200 <vishnuko200@gmail.com>
2021-08-02 10:39:53 +0530

    logo changed

--- src/components/StatsCard.svelte
+++ src/components/StatsCard.svelte
@@ -7,8 +7,8 @@
         <img class="h-v50" src="./coverImage.png" alt="cover"/>
     </div>
     <div class="flex flex-col justify-around md:flex-row">
-    <div class="stats md:mt-0 mt-12"><h1>350+</h1><h3 class="text-xl text-gray-500 mt-4" style="letter-spacing:5px" >COMPONENTS</h3></div>
-        <div class="stats md:mt-0 mt-9"><h1>50+</h1><h3 class="text-gray-500 text-xl mt-4"style="letter-spacing:5px">ICONS</h3></div>
+    <div class="stats md:mt-0 mt-12"><h1>240+</h1><h3 class="text-xl text-gray-500 mt-4" style="letter-spacing:5px" >COMPONENTS</h3></div>
+        <div class="stats md:mt-0 mt-9"><h1>170+</h1><h3 class="text-gray-500 text-xl mt-4"style="letter-spacing:5px">ICONS</h3></div>
         <div class="stats md:mt-0 mt-9"><h1>200+</h1><h3 class="text-gray-500 text-xl mt-4" style="letter-spacing:5px">SCREENS</h3></div>
     </div>
 </div>
@@ -21,6 +21,6 @@
     @apply flex flex-col text-center;
 }
 .stats > h1 {
-    @apply font-black md:text-6xl text-xl bg-gradient-to-t bg-clip-text text-transparent from-black to-white;
+    @apply font-black md:text-6xl text-5xl bg-gradient-to-t bg-clip-text text-transparent from-black to-white;
 }
 </style>
