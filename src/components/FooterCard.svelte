cc06ca4cd630ff834ec135438c065f17450fd727
Ko200 <vishnuko200@gmail.com>
2021-08-25 09:05:55 +0530

    crafted - small case

--- src/components/FooterCard.svelte
+++ src/components/FooterCard.svelte
@@ -1,7 +1,7 @@
 <script>
 </script>
 <div class="order-4 md:order-3 flex md:ml-24 ml-4 md:text-2xl text-xl  font-bold text-gray-300 py-8">
-<span style='letter-spacing'>Crafted with love <span class="text-red-600">&#10084;</span> and sulaimani</span><img class="h-4 mt-2 pl-2" src="./sulaimani.png" alt="tea"/>
+<span style='letter-spacing'>crafted with love <span class="text-red-600">&#10084;</span> and sulaimani</span><img class="h-4 mt-2 pl-2" src="./sulaimani.png" alt="tea"/>
 </div>
 <style lang="postcss">
 </style>
